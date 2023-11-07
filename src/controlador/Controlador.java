76b1a59c64843ce2da69a0a8b1c179c897265a53
Juan Carlos <Juancarmgc2017@gmail.com>
2023-11-07 21:55:35 +0100

    commit con gran actualizacion he creado un metodo en la clase controlador el cual si le metes la ruta de una de las ciudades en este caso Zaragoza te pinta por pantalla y se guarda en un json la informacion de la ciudad

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,5 +1,53 @@
 package controlador;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 public class Controlador {
 
-}
+
+	    public static void main(String[] args) {
+	        try {
+	            // URL de la ciudad de ejemplo
+	            String cityURL = "https://worldweather.wmo.int/es/json/1240_es.xml";
+
+	            // Obtener datos climáticos de la ciudad
+	            String weatherData = fetchDataFromURL(cityURL);
+
+	            // Parsear la información y guardarla como JSON
+	            ObjectMapper objectMapper = new ObjectMapper();
+	            objectMapper.writeValue(new File("city_weather.json"), objectMapper.readValue(weatherData, Object.class));
+	            System.out.println("Datos climáticos guardados en city_weather.json");
+	            
+	            System.out.println(weatherData);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	       
+	    }
+
+	    private static String fetchDataFromURL(String url) throws IOException {
+	        URL weatherURL = new URL(url);
+	        HttpURLConnection connection = (HttpURLConnection) weatherURL.openConnection();
+	        connection.setRequestMethod("GET");
+	        connection.connect();
+
+	        StringBuilder result = new StringBuilder();
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            result.append(line);
+	        }
+	        reader.close();
+
+	        return result.toString();
+	    }
+	}
+
