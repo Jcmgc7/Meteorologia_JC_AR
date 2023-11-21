923dd9a5ea5f766f1d540f6a296ea497e68d1558
Antonio <ruizcabrera186@gmail.com>
2023-11-21 09:38:51 +0100

    botones transparentes to guapos

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -13,7 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 import java.awt.Font;
-
+import java.awt.Insets;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -113,14 +113,26 @@ public class Vista extends JFrame {
 		cartago = new JButton("");
 		cartago.setBounds(354, 353, 36, 31);
 		contentPane.add(cartago);
+		cartago.setMargin(new Insets(0, 0, 0, 0)); 
+		cartago.setContentAreaFilled(false); 
+		cartago.setBorderPainted(false); 
+		cartago.setFocusPainted(false);
 		
-		cordoba = new JButton("");
+		 cordoba = new JButton("");
 		cordoba.setBounds(195, 348, 36, 31);
 		contentPane.add(cordoba);
+		cordoba.setMargin(new Insets(0, 0, 0, 0)); 
+		cordoba.setContentAreaFilled(false); 
+		cordoba.setBorderPainted(false); 
+		cordoba.setFocusPainted(false);
 		
-		ceuta = new JButton("");
+		 ceuta = new JButton("");
 		ceuta.setBounds(143, 481, 36, 31);
 		contentPane.add(ceuta);
+		ceuta.setMargin(new Insets(0, 0, 0, 0)); 
+		ceuta.setContentAreaFilled(false); 
+		ceuta.setBorderPainted(false); 
+		ceuta.setFocusPainted(false);
 		
 		JLabel lblNewLabel_1_2_1 = new JLabel("Nombre Ciudad:");
 		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.BOLD, 12));
@@ -164,121 +176,244 @@ public class Vista extends JFrame {
 		tiempo_E.setBounds(34, 596, 145, 26);
 		contentPane.add(tiempo_E);
 		
-		 santa_cruz = new JButton("");
+		santa_cruz = new JButton("");
 		santa_cruz.setBounds(427, 455, 36, 31);
 		contentPane.add(santa_cruz);
+		santa_cruz.setMargin(new Insets(0, 0, 0, 0)); 
+		santa_cruz.setContentAreaFilled(false); 
+		santa_cruz.setBorderPainted(false); 
+		santa_cruz.setFocusPainted(false);
 		
 		 palma = new JButton("");
 		palma.setBounds(519, 260, 36, 31);
 		contentPane.add(palma);
+		palma.setMargin(new Insets(0, 0, 0, 0)); 
+		palma.setContentAreaFilled(false); 
+		palma.setBorderPainted(false); 
+		palma.setFocusPainted(false);
 		
 		 merida = new JButton("");
 		merida.setBounds(117, 304, 36, 31);
 		contentPane.add(merida);
+		merida.setMargin(new Insets(0, 0, 0, 0)); 
+		merida.setContentAreaFilled(false); 
+		merida.setBorderPainted(false); 
+		merida.setFocusPainted(false);
 		
 		 palma_canaria = new JButton("");
 		palma_canaria.setBounds(480, 486, 36, 31);
 		contentPane.add(palma_canaria);
+		palma_canaria.setMargin(new Insets(0, 0, 0, 0)); 
+		palma_canaria.setContentAreaFilled(false); 
+		palma_canaria.setBorderPainted(false); 
+		palma_canaria.setFocusPainted(false);
+		
 		
 		 alacant = new JButton("");
 		alacant.setBounds(411, 326, 36, 31);
 		contentPane.add(alacant);
+		alacant.setMargin(new Insets(0, 0, 0, 0)); 
+		alacant.setContentAreaFilled(false); 
+		alacant.setBorderPainted(false); 
+		alacant.setFocusPainted(false);
 		
 		 caceres = new JButton("");
 		caceres.setBounds(132, 242, 36, 31);
 		contentPane.add(caceres);
+		caceres.setMargin(new Insets(0, 0, 0, 0)); 
+		caceres.setContentAreaFilled(false); 
+		caceres.setBorderPainted(false); 
+		caceres.setFocusPainted(false);
 		
 		 melilla = new JButton("");
 		melilla.setBounds(292, 501, 36, 31);
 		contentPane.add(melilla);
+		melilla.setMargin(new Insets(0, 0, 0, 0)); 
+		melilla.setContentAreaFilled(false); 
+		melilla.setBorderPainted(false); 
+		melilla.setFocusPainted(false);
 		
 		 madrid = new JButton("");
 		madrid.setBounds(248, 201, 36, 31);
 		contentPane.add(madrid);
+		madrid.setMargin(new Insets(0, 0, 0, 0)); 
+		madrid.setContentAreaFilled(false); 
+		madrid.setBorderPainted(false); 
+		madrid.setFocusPainted(false);
 		
 		 huesca = new JButton("");
 		huesca.setBounds(411, 92, 36, 31);
 		contentPane.add(huesca);
+		huesca.setMargin(new Insets(0, 0, 0, 0)); 
+		huesca.setContentAreaFilled(false); 
+		huesca.setBorderPainted(false); 
+		huesca.setFocusPainted(false);
 		
 		 salamanca = new JButton("");
 		salamanca.setBounds(132, 169, 36, 31);
 		contentPane.add(salamanca);
+		salamanca.setMargin(new Insets(0, 0, 0, 0)); 
+		salamanca.setContentAreaFilled(false); 
+		salamanca.setBorderPainted(false); 
+		salamanca.setFocusPainted(false);
 		
 		 burgos = new JButton("");
 		burgos.setBounds(248, 85, 36, 31);
 		contentPane.add(burgos);
+		burgos.setMargin(new Insets(0, 0, 0, 0)); 
+		burgos.setContentAreaFilled(false); 
+		burgos.setBorderPainted(false); 
+		burgos.setFocusPainted(false);
 		
 		 gerona = new JButton("");
 		gerona.setBounds(540, 80, 36, 31);
 		contentPane.add(gerona);
+		gerona.setMargin(new Insets(0, 0, 0, 0)); 
+		gerona.setContentAreaFilled(false); 
+		gerona.setBorderPainted(false); 
+		gerona.setFocusPainted(false);
 		
 		 valladolid = new JButton("");
 		valladolid.setBounds(195, 128, 36, 31);
 		contentPane.add(valladolid);
+		valladolid.setMargin(new Insets(0, 0, 0, 0)); 
+		valladolid.setContentAreaFilled(false); 
+		valladolid.setBorderPainted(false); 
+		valladolid.setFocusPainted(false);
 		
 		 leon = new JButton("");
 		leon.setBounds(143, 64, 36, 31);
 		contentPane.add(leon);
+		leon.setMargin(new Insets(0, 0, 0, 0)); 
+		leon.setContentAreaFilled(false); 
+		leon.setBorderPainted(false); 
+		leon.setFocusPainted(false);
 		
 		 logrono = new JButton("");
 		logrono.setBounds(292, 92, 36, 31);
 		contentPane.add(logrono);
+		logrono.setMargin(new Insets(0, 0, 0, 0)); 
+		logrono.setContentAreaFilled(false); 
+		logrono.setBorderPainted(false); 
+		logrono.setFocusPainted(false);
 		
 		 pamplona = new JButton("");
 		pamplona.setBounds(338, 56, 36, 31);
 		contentPane.add(pamplona);
+		pamplona.setMargin(new Insets(0, 0, 0, 0)); 
+		pamplona.setContentAreaFilled(false); 
+		pamplona.setBorderPainted(false); 
+		pamplona.setFocusPainted(false);
 		
 		 cadiz = new JButton("");
 		cadiz.setBounds(132, 440, 36, 31);
 		contentPane.add(cadiz);
+		cadiz.setMargin(new Insets(0, 0, 0, 0)); 
+		cadiz.setContentAreaFilled(false); 
+		cadiz.setBorderPainted(false); 
+		cadiz.setFocusPainted(false);
 		
 		 oviedo = new JButton("");
 		oviedo.setBounds(143, 23, 36, 31);
 		contentPane.add(oviedo);
+		oviedo.setMargin(new Insets(0, 0, 0, 0)); 
+		oviedo.setContentAreaFilled(false); 
+		oviedo.setBorderPainted(false); 
+		oviedo.setFocusPainted(false);
 		
 		santiago = new JButton("");
 		santiago.setBounds(21, 39, 36, 31);
 		contentPane.add(santiago);
+		santiago.setMargin(new Insets(0, 0, 0, 0)); 
+		santiago.setContentAreaFilled(false); 
+		santiago.setBorderPainted(false); 
+		santiago.setFocusPainted(false);
 		
 		 valencia = new JButton("");
 		valencia.setBounds(391, 260, 36, 31);
 		contentPane.add(valencia);
+		valencia.setMargin(new Insets(0, 0, 0, 0)); 
+		valencia.setContentAreaFilled(false); 
+		valencia.setBorderPainted(false); 
+		valencia.setFocusPainted(false);
 		
 		 santander = new JButton("");
 		santander.setBounds(235, 23, 36, 31);
 		contentPane.add(santander);
+		santander.setMargin(new Insets(0, 0, 0, 0)); 
+		santander.setContentAreaFilled(false); 
+		santander.setBorderPainted(false); 
+		santander.setFocusPainted(false);
 		
 		 zaragoza = new JButton("");
 		zaragoza.setBounds(365, 128, 36, 31);
 		contentPane.add(zaragoza);
+		zaragoza.setMargin(new Insets(0, 0, 0, 0)); 
+		zaragoza.setContentAreaFilled(false); 
+		zaragoza.setBorderPainted(false); 
+		zaragoza.setFocusPainted(false);
+		
 		
 		 pais_vasco = new JButton("");
 		pais_vasco.setBounds(292, 39, 36, 31);
 		contentPane.add(pais_vasco);
+		pais_vasco.setMargin(new Insets(0, 0, 0, 0)); 
+		pais_vasco.setContentAreaFilled(false); 
+		pais_vasco.setBorderPainted(false); 
+		pais_vasco.setFocusPainted(false);
 		
 		 sevilla = new JButton("");
 		sevilla.setBounds(143, 382, 36, 31);
 		contentPane.add(sevilla);
+		sevilla.setMargin(new Insets(0, 0, 0, 0)); 
+		sevilla.setContentAreaFilled(false); 
+		sevilla.setBorderPainted(false); 
+		sevilla.setFocusPainted(false);
+	
 		
 		 ciudad_real = new JButton("");
 		ciudad_real.setBounds(235, 294, 36, 31);
 		contentPane.add(ciudad_real);
+		ciudad_real.setMargin(new Insets(0, 0, 0, 0)); 
+		ciudad_real.setContentAreaFilled(false); 
+		ciudad_real.setBorderPainted(false); 
+		ciudad_real.setFocusPainted(false);
+	
 		
 		 albacete = new JButton("");
 		albacete.setBounds(324, 299, 36, 31);
 		contentPane.add(albacete);
+		albacete.setMargin(new Insets(0, 0, 0, 0)); 
+		albacete.setContentAreaFilled(false); 
+		albacete.setBorderPainted(false); 
+		albacete.setFocusPainted(false);
+	
 		
 		 tarragona = new JButton("");
 		tarragona.setBounds(457, 148, 36, 31);
 		contentPane.add(tarragona);
+		tarragona.setMargin(new Insets(0, 0, 0, 0)); 
+		tarragona.setContentAreaFilled(false); 
+		tarragona.setBorderPainted(false); 
+		tarragona.setFocusPainted(false);
+		
+		
 		
 		 toledo = new JButton("");
 		toledo.setBounds(235, 242, 36, 31);
 		contentPane.add(toledo);
+		toledo.setMargin(new Insets(0, 0, 0, 0)); 
+		toledo.setContentAreaFilled(false); 
+		toledo.setBorderPainted(false); 
+		toledo.setFocusPainted(false);
 		
 		barcelona = new JButton("");
 		barcelona.setBounds(506, 112, 36, 31);
 		contentPane.add(barcelona);
+		barcelona.setMargin(new Insets(0, 0, 0, 0)); 
+		barcelona.setContentAreaFilled(false); 
+		barcelona.setBorderPainted(false); 
+		barcelona.setFocusPainted(false);
 		
 		listaComunidad = new JComboBox();
 		listaComunidad.setBounds(722, 224, 116, 31);
